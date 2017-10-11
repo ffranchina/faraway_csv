d2ac718a8137ba238b6be73acb0f40ca45da8657
Francesco Franchina <cescus92@gmail.com>
2017-10-11 11:36:31 +0200

    Converted datasets to .csv format

---
+++ convert.R
@@ -0,0 +1,7 @@
+args <- commandArgs(trailingOnly=TRUE)
+
+rda_filename <- args[1]
+data_name <- load(rda_filename)
+csv_filename <- paste(c(data_name, 'csv'), collapse='.')
+
+write.csv(get(data_name), csv_filename)
